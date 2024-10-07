59dfe2cf5ac68203290a371922dd72508370c9d9
Vivek Namdev <viveknamdev257@gmail.com>
2024-10-07 14:00:49 +0530

    adding bcrypt and zod library

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const bcrypt = require('bcrypt');
 const {UserModel, TodoModel} = require("./db");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const JWT_SECRET = "asdasd123@123";
-
 
 mongoose.connect("mongodb+srv://viveknamdev018:8e3ToWcBwTMIOMTP@cluster0.r2qd2.mongodb.net/todo-vivek-2222");
 const app = express();
@@ -13,7 +13,7 @@ app.post("/signup", async function(req, res) {
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
-
+    
     await UserModel.create({
         email: email,
         password: password,
